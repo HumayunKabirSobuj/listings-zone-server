eaf9e2da3138e65d9f516d02321718ef335170f8
HumayunKabirSobuj <humayunkabirgithub@gmail.com>
2024-09-12 10:36:54 +0600

    complete

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://localhost:5174",
+      "https://zone-club.netlify.app",
       
     ],
     credentials: true,
@@ -16,7 +17,7 @@ app.use(
 
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // const uri =
 //   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.o9b6e9v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri = process.env.DB_URI;
@@ -32,30 +33,98 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db("TaskDB");
     const usersCollection = database.collection("usersCollection");
+    const listingsCollection = database.collection("listingsCollection");
 
-    app.post('/users', async(req, res)=>{
-        const userInfo=req.body;
-        console.log(userInfo)
-        const result = await usersCollection.insertOne(userInfo);
-        res.send(result)
-    })
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      // console.log(userInfo)
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result);
+    });
 
-    app.get('/users', async(req, res)=>{
-        const result = await usersCollection.find().toArray();
-        res.send(result);
-    })
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.post("/listings", async (req, res) => {
+      const data = req.body;
+      // console.log(data)
+      const result = await listingsCollection.insertOne(data);
+      res.send(result);
+    });
 
+    app.get("/listings", async (req, res) => {
+      const result = await listingsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.delete("/listings/:id", async (req, res) => {
+      const id = req.params.id;
+      // console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await listingsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // Update Related work
+
+    app.get("/listings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await listingsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.patch("/listings/edit/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedListing = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const listing = {
+        $set: {
+          Name: updatedListing.Name,
+          Description: updatedListing.Description,
+          Photo: updatedListing.Photo,
+        },
+      };
+
+      const result = await listingsCollection.updateOne(
+        filter,
+        listing,
+        options
+      );
+      res.send(result);
+    });
+
+    app.patch("/listings/active/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedListing = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const listing = {
+        $set: {
+          ActiveStatus: updatedListing.ActiveStatus,
+        },
+      };
+      const result = await listingsCollection.updateOne(
+        filter,
+        listing,
+        options
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -69,4 +138,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
